1c51ab84bf467d5f9cd9b26cd158a3b8ccdbcf7d
Partoo Hwong <partoo@163.com>
2016-08-24 20:44:05 +0800

    init

---
+++ resources/views/layouts/overlay.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+    <div class="overlay_open overlay vcenter">
+        <div class="overlay_mask"></div>
+        <div class="overlay_content">
+            @yield('content')
+        </div>
+    </div>
